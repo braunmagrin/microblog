5ad1956d60fea73254fbe7b6e93de81fb8b615dc
Matheus Magrin <braunmagrin@gmail.com>
2014-12-07 13:28:17 -0200

    Uses loop in template

--- app/views.py
+++ app/views.py
@@ -5,5 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Matheus'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
     return render_template('index.html',
-                           user=user)
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
